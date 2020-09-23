f6fa7a22313e66d7270a44d6c4fc17b666382e08
Virgil Rosalita <shekinahberry143@gmail.com>
2020-09-23 11:13:58 +0800

    Adding comments to this model

--- app/Country.php
+++ app/Country.php
@@ -1,5 +1,8 @@
 <?php
 
+/* Note: Some of the models refers to different databases in mysql because this project uses multiple mysql database */
+/* This is the model for the countries in the database */
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
